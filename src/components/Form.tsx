af9f4fcd730d06930a1b3b97f1be960fb1e507e0
Jose Horacio <jhoracio19@hotmail.com>
2026-05-24 22:46:38 -0600

    definiendo el reducer

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,4 @@
-import { useState, type ChangeEvent } from "react"
+import { useState, type ChangeEvent, type FormEvent } from "react"
 import type { Activity } from "../types"
 import { categories } from "../data/categories"
 
@@ -20,13 +20,19 @@ export default function Form() {
 
     const isValidActivity = () => {
         const {name, calories} = activity
-        console.log(name.trim() !== '' && calories > 0);
         return name.trim() !== '' && calories > 0
+    }
+
+    const handleSubmit = (e : FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        console.log('Submit...');
     }
 
   return (
     <form
     className="space-y-5 bg-white shadow p-10 rounded-lg"
+    onSubmit={handleSubmit}
     >
         <div className="grid grid-cols-1 gap-3">
             <label 
@@ -92,7 +98,7 @@ export default function Form() {
         <input 
         type="submit"
         className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10"
-        value="Guardar Comida o Guardar Ejercicio"
+        value={activity.category === 1 ? 'Guardar Comida' : 'Guardar Ejericio'}
         disabled={!isValidActivity()}
         />
 
